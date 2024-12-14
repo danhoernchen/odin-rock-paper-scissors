98deded72a4cd523b326121aef7a356a45de6ba7
Daniel <191582347+danhoernchen@users.noreply.github.com>
2024-12-14 21:16:55 +0100

    Computers choice and players choice
    Added functions to make a random computer choice and get input for the users choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
-console.log('Hello World!')
+function getComputerChoice(){
+    let number = Math.floor((Math.random() * 3) + 1);
+    if (number === 1){
+        return "rock";
+    } else if (number === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    choice = prompt("What's your choice: Rock, Paper or Scissors?")
+    choice = choice.trim().toLowerCase();
+    while (true){
+    if(choice === "rock") {
+        return "rock";
+    } else if (choice === "paper"){
+        return "paper";
+    } else if (choice === "scissors"){
+        return "scissors"
+    } else {
+        console.log ("Invalid choice, please enter either Rock, Paper or Scissors.");
+        getHumanChoice();
+    }
+}
+}
+
+function checkInput(choice) {
+
+}
