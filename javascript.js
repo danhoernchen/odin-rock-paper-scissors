41c17e6678c347a5d1c3b35a709ce97da29c2671
Daniel <191582347+danhoernchen@users.noreply.github.com>
2024-12-15 12:36:09 +0100

    Player choice Buttons
    added Buttons to html and Eventhandlers in javascript to get the players choice

--- javascript.js
+++ javascript.js
@@ -23,12 +23,38 @@ function getHumanChoice() {
       console.log(
         "Invalid choice, please enter either Rock, Paper or Scissors."
       );
-      getHumanChoice();
     }
   }
 }
 
-function playGame() {
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+rockBtn.addEventListener("click", playRound);
+paperBtn.addEventListener("click", playRound);
+scissorsBtn.addEventListener("click", playRound);
+
+var humanScore = 0;
+var computerScore = 0;
+function playRound() {
+  var computer = getComputerChoice();
+  var human = this.value;
+  if (
+    (computer === "Rock" && human === "Scissors") ||
+    (computer === "Paper" && human === "Rock") ||
+    (computer === "Scissors" && human === "Paper")
+  ) {
+    computerScore++;
+    console.log(`You lose the round! ${computer} beats ${human}.`);
+  } else if (human === computer) {
+    console.log(`It's a draw. Both chose ${human}`);
+  } else {
+    humanScore++;
+    console.log(`You win the round! ${human} beats ${computer}.`);
+  }
+}
+
+/* function playGame() {
   var humanScore = 0;
   var computerScore = 0;
   while (humanScore < 5 && computerScore < 5) {
@@ -57,4 +83,4 @@ function playGame() {
       console.log(`You win the round! ${human} beats ${computer}.`);
     }
   }
-}
+} */
