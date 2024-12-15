736cc1b48bfb41878587ffa3be6abd44a7abec06
Daniel <191582347+danhoernchen@users.noreply.github.com>
2024-12-15 13:52:19 +0100

    reset button, game-logic, score display
    added logic for a best of 5 game
    added reset button
    added score display and current round display

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
+/* function getHumanChoice() {
   choice = prompt("What's your choice: Rock, Paper or Scissors?");
   choice = choice.trim().toLowerCase();
   while (true) {
@@ -25,14 +25,18 @@ function getHumanChoice() {
       );
     }
   }
-}
+} */
 
 const rockBtn = document.getElementById("rockBtn");
 const paperBtn = document.getElementById("paperBtn");
 const scissorsBtn = document.getElementById("scissorsBtn");
+const compScoreDisplay = document.getElementById("computerScore");
+const humanScoreDisplay = document.getElementById("humanScore");
+const roundTextDisplay = document.getElementById("roundText");
 rockBtn.addEventListener("click", playRound);
 paperBtn.addEventListener("click", playRound);
 scissorsBtn.addEventListener("click", playRound);
+resetBtn.addEventListener("click", resetGame);
 
 var humanScore = 0;
 var computerScore = 0;
@@ -45,15 +49,40 @@ function playRound() {
     (computer === "Scissors" && human === "Paper")
   ) {
     computerScore++;
-    console.log(`You lose the round! ${computer} beats ${human}.`);
+    compScoreDisplay.innerText = computerScore;
+    roundTextDisplay.innerText = `You lose the round! ${computer} beats ${human}.`;
   } else if (human === computer) {
-    console.log(`It's a draw. Both chose ${human}`);
+    roundTextDisplay.innerText = `It's a draw. Both chose ${human}`;
   } else {
     humanScore++;
-    console.log(`You win the round! ${human} beats ${computer}.`);
+    humanScoreDisplay.innerText = humanScore;
+    roundTextDisplay.innerText = `You win the round! ${human} beats ${computer}.`;
+  }
+  if (humanScore >= 5 || computerScore >= 5) {
+    rockBtn.classList.add("disabled");
+    paperBtn.classList.add("disabled");
+    scissorsBtn.classList.add("disabled");
+    resetBtn.classList.remove("disabled");
   }
 }
 
+function gameOverDisplay() {
+  if (humanScore >= 5) {
+    roundTextDisplay.innerText = "Congratulations, you won the game.";
+  } else {
+    roundTextDisplay.innerText =
+      "Better luck next time, the computer won this game.";
+  }
+}
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  rockBtn.classList.remove("disabled");
+  paperBtn.classList.remove("disabled");
+  scissorsBtn.classList.remove("disabled");
+  resetBtn.classList.add("disabled");
+}
 /* function playGame() {
   var humanScore = 0;
   var computerScore = 0;
